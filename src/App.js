8b86a1148191c90c9386769030e9e13ac550fa39
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-12 02:56:35 +0600

    services section done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
+import Contact from './Pages/Contact/Contact'
+import Reviews from './Pages/Reviews/Reviews'
+import Login from './Pages/Login/Login'
+import Appointment from './Pages/Appointment/Appointment'
+
 
 function App() {
   return (
@@ -12,6 +17,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/contact' element= {<Contact></Contact>}></Route>
+        <Route path = '/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path = '/login' element= {<Login></Login>}></Route>
+        <Route path='/appointment' element= {<Appointment></Appointment>}></Route>
       </Routes>
     </div>
   );
