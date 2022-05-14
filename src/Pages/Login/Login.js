258739a9a4031f349872c97e9532071854d7de52
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-14 15:39:11 +0600

    implement google signin

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user._tokenResponse.displayName)
+    }
     return (
-        <div>
-            <h2>Please login</h2>
+        <div className='flex mt-20 justify-center item-center'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title m-auto text-primary font-bold text-3xl"> Log In</h2>
+                    <div class="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline"> Continue with google</button>
+                </div>
+            </div>
         </div>
     );
 };
