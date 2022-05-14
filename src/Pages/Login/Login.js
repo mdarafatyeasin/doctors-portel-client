317e3ba357a331f493f7022a1c0a964c7628bc3d
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-14 17:08:18 +0600

    implementing the login form from react form hookes,  basic email in done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,71 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init'
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    if(user){
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    if (user) {
         console.log(user._tokenResponse.displayName)
     }
+
+    const onSubmit = data => {
+
+
+        console.log(data)
+    };
+
     return (
         <div className='flex mt-20 justify-center item-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title m-auto text-primary font-bold text-3xl"> Log In</h2>
-                    <div class="divider">OR</div>
+                <div className="card-body">
+                    <h2 className="card-title m-auto text-primary font-bold text-3xl"> Log In</h2>
+
+                    {/* ----------------------------Email and password--------------------------- */}
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        {/* -------------------------------------------------------------------- */}
+
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Email:</span>
+                            </label>
+                            <input 
+                            // register came from react hookes form
+                            {...register("email", {
+                                pattern: {
+                                  value: /[A-Za-z]{3}/,
+                                  message: 'error message'
+                                }
+                              })} 
+                            type="email" placeholder="Enter your email" 
+                            class="input input-bordered w-full max-w-xs" 
+                            />
+                            <label class="label">
+                                <span class="label-text-alt">Alt label</span>
+                            </label>
+                        </div>
+
+
+                        {/* -------------------------------------------------------------------- */}
+                        <input  />
+                        {errors.firstName?.type === 'required' && "First name is required"}
+
+                        <input {...register("lastName", { required: true })} />
+                        {errors.lastName && "Last name is required"}
+
+                        <input type="submit" />
+                    </form>
+
+                    {/* -------------------------------------------------------------------------- */}
+
+                    <div className="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
-                        class="btn btn-outline"> Continue with google</button>
+                        className="btn btn-outline"> Continue with google</button>
                 </div>
             </div>
         </div>
