a076e6a1b11ccb17158564aa823b7807784e0182
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-15 07:26:19 +0600

    tailwind css spnner added

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
-            <button className="btn btn-square loading"></button>
+            <div class="flex items-center justify-center ">
+                <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
+            </div>
         </div>
     );
 };
