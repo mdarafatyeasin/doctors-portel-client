feec1ee29ef323248aaf3c8490fce747c1250021
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-15 13:48:24 +0600

    all done

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import './Loading.css'
 
 const Loading = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
-            <div class="flex items-center justify-center ">
-                <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
-            </div>
+            <span class="loader"></span>  
         </div>
     );
 };
